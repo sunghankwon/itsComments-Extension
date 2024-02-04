7121accef3a1ff9a1a3050158e69c202a16112e1
JungDeunGyul <ask7384@gmail.com>
2024-02-05 01:20:45 +0900

    feat:Link to userURL

---
+++ src/components/UserComment/index.jsx
@@ -0,0 +1,19 @@
+function UserComment({ user }) {
+  return (
+    <div className="mt-6 shadow-lg shadow-red-500 md:shadow-xl">
+      <img
+        src={user.icon}
+        className="h-16 w-16 object-cover rounded-full p-[3px] border-gray-300 hover:bg-slate-200"
+      />
+      <p>{user.name}</p>
+      <p>{user.comment}</p>
+      <a href={user.imgUrl} target="_black" rel="noopener noreferrer">
+        <button className="bg-violet-200 hover:bg-violet-400 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full">
+          URL 링크
+        </button>
+      </a>
+    </div>
+  );
+}
+
+export default UserComment;
