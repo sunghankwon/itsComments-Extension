f3aa7fbf9cbbe5328bed8570115656dd8b8c5411
sunghankwon <akvo852@naver.com>
2024-02-05 00:31:30 +0900

    feat: add Login componts

---
+++ src/components/App.jsx
@@ -0,0 +1,6 @@
+import Login from "./Login";
+
+function App() {
+  return <Login />;
+}
+export default App;
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div className="flex items-center justify-center">
+      <button className="bg-blue-500 text-white px-4 py-2 mt-[100px] rounded hover:bg-blue-700">
+        Login with Google
+      </button>
+    </div>
+  );
+}
+
+export default Login;
