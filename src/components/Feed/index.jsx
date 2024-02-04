cb3037e0be887c65b6cb1543558686aca781e03a
JungDeunGyul <ask7384@gmail.com>
2024-02-05 04:52:34 +0900

    style:set veriable name indenting

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -3,21 +3,19 @@ import { useState, useEffect } from "react";
 import UserComment from "../UserComment";
 import axios from "axios";
 
-const REACT_APP_BACKEND_URI = process.env.SOME_KEY;
-
 function Feed() {
   const [userComments, setUserComment] = useState([]);
 
   useEffect(() => {
     const fetchComments = async () => {
       try {
-        const response = await axios.get(REACT_APP_BACKEND_URI, {
+        const response = await axios.get(import.meta.env.BACKEND_KEY, {
           withCredentials: true,
         });
 
         setUserComment(response.data);
-      } catch (err) {
-        console.err("Error fetching comments: ", err);
+      } catch (error) {
+        console.error("Error fetching comments: ", error);
       }
     };
     fetchComments();
