ef755b3047e079c8f2cd1611979c21fd010923ae
sunghankwon <akvo852@naver.com>
2024-02-24 19:51:03 +0900

    refactor: Remove unused token state management

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,9 @@ import Header from "./Header/Header";
 import Feed from "./Feed";
 
 import useUserStore from "../store/userProfile";
-import useAuthTokenStore from "../store/useToken";
 
 function App() {
   const { userData, setUserData } = useUserStore();
-  const { setAuthToken } = useAuthTokenStore();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -25,7 +23,6 @@ function App() {
             { withCredentials: true },
           );
 
-          setAuthToken(authToken);
           setUserData(res.data.user);
 
           chrome.runtime.sendMessage({
--- src/store/useToken.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useAuthTokenStore = create((set) => ({
-  authToken: null,
-  setAuthToken: (data) => set(() => ({ authToken: data })),
-}));
-
-export default useAuthTokenStore;
