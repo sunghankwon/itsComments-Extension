0e253e1d1289a1d4a644f8cf79940ed67a2288e1
sunghankwon <akvo852@naver.com>
2024-04-27 18:56:50 +0900

    fix: Add auto-scroll functionality to extension on URL navigation

--- src/components/FeedComment/index.jsx
+++ src/components/FeedComment/index.jsx
@@ -1,4 +1,5 @@
 function FeedComment({ comment, onRemoveComment }) {
+  const commentPost = parseInt(comment.postCoordinate.y, 10) - 100;
   const openCommentTab = () => {
     chrome.runtime.sendMessage({
       action: "openCommentTab",
@@ -17,28 +18,28 @@ function FeedComment({ comment, onRemoveComment }) {
 
   return (
     <div className="w-[250px] mt-3 mb-3 border border-blue-500 rounded-md shadow-lg shadow-blue-500 md:shadow-xl">
-      <div className="flex mt-2 items-center">
+      <div className="flex items-center mt-2">
         <img
           src={comment.creator.icon}
           className="h-8 w-8 ml-2 object-cover rounded-full p-[3px] border-gray-300 hover:bg-slate-200 flex-shrink-0"
         />
-        <span className="text-xl text-white ml-2">
+        <span className="ml-2 text-xl text-white">
           {comment.creator.nickname}
         </span>
       </div>
       <p className="ml-2 text-base text-white">{comment.text}</p>
       <img onClick={handleImageClick} src={comment.screenshot} />
-      <div className="flex ml-2 mt-2 mb-2 items-center">
+      <div className="flex items-center mt-2 mb-2 ml-2">
         <span className="text-white">
           {comment.allowPublic === true ? "공개" : "비공개"}
         </span>
         <a
-          href={comment.postUrl}
+          href={`${comment.postUrl}?scroll=${commentPost}`}
           onClick={handleLinkClick}
           target="_black"
           rel="noopener noreferrer"
         >
-          <button className="ml-4 bg-blue-200 hover:bg-blue-400 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 rounded-md">
+          <button className="ml-4 bg-blue-200 rounded-md hover:bg-blue-400 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
             URL 링크
           </button>
         </a>
