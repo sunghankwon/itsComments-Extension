b8cb0b519eacec368f6444c84dfd4f094e3071a8
sunghankwon <akvo852@naver.com>
2024-06-05 16:40:06 +0900

    chore: Remove incorrect whitespace

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -47,7 +47,7 @@ function Login() {
       </div>
       <div className="flex flex-col mt-[30px] items-center">
         <p className="mt-2 text-3xl text-white">Welcome to</p>
-        <p className="mt-2 text-4xl text-white">Its Comments! </p>
+        <p className="mt-2 text-4xl text-white">Its Comments!</p>
         <button
           onClick={handleLogin}
           className="w-11/12 bg-blue-500 text-white px-4 py-2 mt-[30px] mb-[100px] rounded hover:bg-blue-700"
